06495a58ed0cf449027b6573b41909b075450460
loki <31621774+lokendramorda@users.noreply.github.com>
2025-04-21 14:28:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,9 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({
-  origin: 'http://localhost:3000'  // Frontend URL
+  origin: ['http://localhost:3000', 'https://goldprices.netlify.app']  // Frontend URLs
 }));
+
 
 // MongoDB URI and client setup
 const uri = process.env.MONGO_URI;
